4f65d34e89410b8be8113adb506e052ec19c7f9a
Wyatt Lansford <wlans4@vt.edu>
2019-04-30 19:46:10 -0400

    Update image_capture.py

--- image_capture.py
+++ image_capture.py
@@ -1,33 +1,62 @@
 import cv2
+import glob
+import os
 
-LEFT_PATH = "C:/Users/example/camera_capture/LEFT/{:03d}.jpg"
-RIGHT_PATH = "C:/Users/example/camera_capture/RIGHT/{:03d}.jpg"
-
+CHESSBOARD_CALIBRATION_SIZE = (5,7)
+IMAGES_TO_TAKE = 200
 left = cv2.VideoCapture(0)
-right = cv2.VideoCapture(2)
+right = cv2.VideoCapture(1)
+DRAW_IMAGE = True
 
-frame_count = 1
+# Create the paths to store images if they do not exist yet
+path = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+LEFT_PATH = path + "LEFT"
+RIGHT_PATH = path + "RIGHT"
 
-while(frame_count <= 25):
+if not os.path.exists(LEFT_PATH):
+    try:
+        os.mkdir(LEFT_PATH)
+    except:
+        pass
+if not os.path.exists(RIGHT_PATH):
+    try:
+        os.mkdir(RIGHT_PATH)
+    except:
+        pass
+
+
+
+LEFT = LEFT_PATH + "{:01d}.png"
+RIGHT = RIGHT_PATH + "{:01d}.png"
+frame_count = 0
+
+#Clear out the directory first
+for f in glob.glob(LEFT_PATH + "/*"):
+    os.remove(f)
+for f in glob.glob(RIGHT_PATH + "/*"):
+    os.remove(f)
+
+
+while True:
 
     if not left.grab() or not right.grab():
         print("Frames not grabbed")
         break
-
     grabbed, left_frame = left.retrieve()
     grabbed, right_frame = right.retrieve()
 
-    cv2.imwrite(LEFT_PATH.format(frame_count), left_frame)
-    cv2.imwrite(RIGHT_PATH.format(frame_count), right_frame)
-
-    cv2.imshow('left', left_frame)
-    cv2.imshow('right', right_frame)
-    frame_count += 1
-
-    # Press escape to quit
-    if cv2.waitKey(33) == 27:
+    if DRAW_IMAGE:
+        cv2.imshow('left', left_frame)
+        cv2.imshow('right', right_frame)
+    key = cv2.waitKey(1)
+    if key%256 == 32:
+        cv2.imwrite(LEFT.format(frame_count), left_frame)
+        cv2.imwrite(RIGHT.format(frame_count), right_frame)
+        frame_count += 1
+    elif key%256 == 27:
+        break
+    if frame_count >= IMAGES_TO_TAKE:
         break
 
 left.release()
 right.release()
-cv2.destroyAllWindows()
